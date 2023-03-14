ebd8f7ee0c61f27dd6739ca76fb3a372843e0d9e
Sofy24 <sofia.tosi4@studio.unibo.it>
2023-03-14 23:10:07 +0100

    + Optionals

--- src/main/scala/u02/Optionals.scala
+++ src/main/scala/u02/Optionals.scala
@@ -20,6 +20,18 @@ object Optionals extends App:
       case Some(a) => f(a)
       case _ => None()
 
+    def filter[A] (opt: Option[A])(predicate: A => Boolean): Option[A] = opt match
+      case Some(a) if predicate(a) => Some(a)
+      case _ => None()
+
+    def map[A] (opt:Option[A])(predicate: A => Boolean): Option[Boolean] = opt match
+      case Some(a) => Option.Some(predicate(a))
+      case _ => Option.None()
+
+    def fold[A] (opt:Option[A])(default: Int)(f: A => Int): Int = opt match
+      case Some(a) => f(a)
+      case None() => default
+
   import Option.*
 
   val s1: Option[Int] = Some(1)
@@ -32,3 +44,11 @@ object Optionals extends App:
   println(flatMap(s1)(i => Some(i + 1))) // Some(2)
   println(flatMap(s1)(i => flatMap(s2)(j => Some(i + j)))) // Some(3)
   println(flatMap(s1)(i => flatMap(s3)(j => Some(i + j)))) // None
+  println(fold(Some(5))(1)(_ + 1)) // 6
+  println(fold(None[Int]())(1)(_ + 1)) // 1
+  println(filter(Some(5))(_ > 2)) // Some(5)
+  println(filter(Some(5))(_ > 8)) // None
+  println(filter(None[Int]())(_ > 2)) // None
+  println(map(Some(5))(_ > 2)) // Some(true)
+  println(map(Some(5))(_ > 8)) // Some(false)
+  println(map(None[Int]())(_ > 2)) // None
